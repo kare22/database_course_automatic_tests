efb4a816a6a3b27086be419b20453f350ce1c9cb
karelpaan <paan.karel@gmail.com>
2023-03-15 16:40:06 +0200

    remake try 1

--- checks.py
+++ checks.py
@@ -1,122 +1,156 @@
+### STATIC METHODS ###
+import os
+
+
+def _responseWrapper(result, points, attributes):
+    response = ""
+
+    if 'tableName' in attributes:
+        response += f"Tabel {attributes['tableName']} "
+    if 'columnName' in attributes:
+        response += f"Veerg {attributes['columnName']} "
+    if 'constraintName' in attributes:
+        response += f"Kitsendus {attributes['constraintName']} "
+    if 'constraintType' in attributes and attributes['constraintType'] is not None:
+        response += f"Kitsendustüüp {attributes['constraintType']} "
+    if 'funcName' in attributes and attributes['funcName'] == 'checkDefault':
+        response += 'Vaikevaartus '
+
+    if 'receivedValue' in attributes and attributes['receivedValue'] is not None:
+        if result:
+            response += f"Väärtus {attributes['receivedValue']} on olemas"
+        else:
+            response += f"Oodati väärtust {attributes['expectedValue']} kuid saadi {attributes['receivedValue']}"
+
+    if ('shouldExist' in attributes) and ('expectedValue' not in attributes or attributes['expectedValue'] is None):
+        if attributes['shouldExist'] == False:
+            if result:
+                response += 'on eemaldatud'
+            else:
+                response += 'eeldati et ei ole kuid on olemas'
+        else:
+            if result:
+                response += 'on olemas'
+            else:
+                response += 'ei ole olemas'
+
+    return result, response, points
+
+
+def getCheckColumnQuery(tableName, columnName, attributeName='*', expectedValue=None, points=0):
+    query = f"SELECT {attributeName} FROM information_schema.columns WHERE table_name = '{tableName}' AND column_name = '{columnName}'"
+
+    return {
+        'query': query,
+        'tableName': tableName,
+        'columnName': columnName,
+        'attributeName': attributeName,
+        'expectedValue': expectedValue,
+        'points': points,
+        'shouldExist': expectedValue is None,
+        'funcName': 'checkColumn',
+    }
+
+    # return self._responseWrapper(check(), {
+    #     'funcName': 'checkConstraint',
+    #     'tableName': tableName,
+    #     'columnName': columnName,
+    #     'constraintName': constraintName,
+    #     'constraintType': constraintType,
+    #     'attributeName': attributeName,
+    #     'expectedValue': expectedValue,
+    # })
+
+
+def getCheckDefaultQuery(tableName, columnName, expectedValue, points=0):
+    query = f"SELECT * FROM information_schema.columns WHERE table_name  = '{tableName}' AND column_name = '{columnName}'"
+
+    if expectedValue is None:
+        query += "AND column_default IS NULL"
+    else:
+        query += f"AND column_default LIKE '%{expectedValue}%'"
+
+    return {
+        'query': query,
+        'tableName': tableName,
+        'expectedValue': expectedValue,
+        'shouldExist': expectedValue != None,
+        'columnName': columnName,
+        'points': points,
+        'funcName': 'checkDefault',
+    }
+
+
+def getCheckConstraintQuery(tableName, constraintName, constraintType=None, shouldNotExist=False, points=0, schema='public'):
+    query = f"SELECT * FROM information_schema.table_constraints WHERE table_name = '{tableName}' AND table_schema = '{schema}' AND constraint_name = '{constraintName}'"
+
+    if constraintType != None:
+        query += f" AND constraint_type = '{constraintType}'"
+
+    return {
+        'tableName': tableName,
+        'constraintName': constraintName,
+        'constraintType': constraintType,
+        'shouldExist': not shouldNotExist,
+        'query': query,
+        'points': points,
+        'funcName': 'checkConstraint',
+    }
+
+
 class Checker:
 
     def __init__(self, schema, cur):
         self.schema = schema
         self.cur = cur
 
-    def _responseWrapper(self, result, points, attributes):
-        response = ""
-
-        if 'tableName' in attributes:
-            response += f"Tabel {attributes['tableName']} "
-        if 'columnName' in attributes:
-            response += f"Veerg {attributes['columnName']} "
-        if 'constraintName' in attributes:
-            response += f"Kitsendus {attributes['constraintName']} "
-        if 'constraintType' in attributes and attributes['constraintType'] is not None:
-            response += f"Kitsendustüüp {attributes['constraintType']} "
-        if 'funcName' in attributes and attributes['funcName'] == 'checkDefault':
-            response += 'Vaikevaartus '
-
-        if 'receivedValue' in attributes and attributes['receivedValue'] is not None:
-            if result:
-                response += f"Väärtus {attributes['receivedValue']} on olemas"
-            else:
-                response += f"Oodati väärtust {attributes['expectedValue']} kuid saadi {attributes['receivedValue']}"
-
-        if ('shouldExist' in attributes) and ('expectedValue' not in attributes or attributes['expectedValue'] is None):
-            if attributes['shouldExist'] == False:
-                if result:
-                    response += 'on eemaldatud'
-                else:
-                    response += 'eeldati et ei ole kuid on olemas'
-            else:
-                if result:
-                    response += 'on olemas'
-                else:
-                    response += 'ei ole olemas'
-
-        return result, response, points
-
-    def checkConstraint(self, tableName, constraintName, constraintType=None, shouldNotExist=False, points=0):
+    def checkConstraint(self, params):
         def check():
             try:
-                query = f"SELECT * FROM information_schema.table_constraints WHERE table_name = '{tableName}' AND table_schema = '{self.schema}' AND constraint_name = '{constraintName}'"
+                print(params['query'])
+                self.cur.execute(params['query'])
 
-                if constraintType != None:
-                    query += f"AND constraint_type = '{constraintType}'"
-
-                self.cur.execute(query)
-
-                return len(self.cur.fetchall()) == (0 if shouldNotExist else 1)
+                return len(self.cur.fetchall()) == (0 if params['shouldNotExist'] else 1)
             except:
                 return False
 
-        return self._responseWrapper(check(), points, {
-            'tableName': tableName,
-            'constraintName': constraintName,
-            'constraintType': constraintType,
-            'shouldExist': not shouldNotExist
-        })
-    
-    def checkDefault(self, tableName, columnName, expectedValue, points=0):
+        return _responseWrapper(check(), params['points'], params)
+
+    def checkDefault(self, params):
         def check():
             try:
-                query = f"SELECT * FROM information_schema.columns WHERE table_name  = '{tableName}' AND column_name = '{columnName}'"
-
-                if expectedValue == None:
-                    query += "AND column_default IS NULL"
-                else:
-                    query += f"AND column_default LIKE '%{expectedValue}%'"
-
-                self.cur.execute(query)
+                self.cur.execute(params['query'])
 
                 return len(self.cur.fetchall()) == 1
             except:
                 return False
 
-        return self._responseWrapper(check(), points, {
-            'tableName': tableName,
-            'expectedValue': expectedValue,
-            'shouldExist': expectedValue != None,
-            'columnName': columnName,
-            'funcName': 'checkDefault',
-        })
-    
-    def checkColumn(self, tableName, columnName, attributeName='*', expectedValue=None, points=0):
+        return _responseWrapper(check(), params['points'], params)
+
+    def checkColumn(self, params):
         def check():
             try:
-                query = f"SELECT {attributeName} FROM information_schema.columns WHERE table_name = '{tableName}' AND column_name = '{columnName}'"
+                self.cur.execute(params['query'])
 
-                self.cur.execute(query)
-
-                if expectedValue == None:
+                if params['expectedValue'] == None:
                     return len(self.cur.fetchall()) > 0, None
 
                 response = self.cur.fetchall()[0][0]
-                return (str(response)) == str(expectedValue), response
+                return (str(response)) == str(params['expectedValue']), response
             except:
                 return False, 'VIGA'
 
         result, receivedValue = check()
 
-        return self._responseWrapper(result, points, {
-            'tableName': tableName,
-            'columnName': columnName,
-            'attributeName': attributeName,
-            'expectedValue': expectedValue,
-            'receivedValue': receivedValue,
-            'shouldExist': expectedValue is None,
-        })
+        return _responseWrapper(result, params['points'], params)
 
-
-
-        # return self._responseWrapper(check(), {
-        #     'funcName': 'checkConstraint',
-        #     'tableName': tableName,
-        #     'columnName': columnName,
-        #     'constraintName': constraintName,
-        #     'constraintType': constraintType,
-        #     'attributeName': attributeName,
-        #     'expectedValue': expectedValue,
-        # })
+    def runTestQuery(self, test):
+        # Should be more dynamic, but python does not have a good solution for calling class methods dynamically
+        if test['funcName'] == 'checkColumn':
+            return self.checkColumn(test)
+        elif test['funcName'] == 'checkDefault':
+            return self.checkDefault(test)
+        elif test['funcName'] == 'checkConstraint':
+            return self.checkConstraint(test)
+        else:
+            raise Exception('No such test found!')
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from uuid import uuid4
 from datetime import datetime
 from dotenv import load_dotenv
 
-from checks import Checker
+from checks import *
 
 # Load environment variables from .env file
 load_dotenv()
@@ -42,7 +42,7 @@ def connect(dbName=os.getenv('DB_NAME'), autoCommit=False):
 
     return connection
 
-def testStudent(testFileName):
+def testStudent(testFileName, tests):
     id = f"temp_{str(uuid4()).replace('-', '_')}"
 
     # Create substitute database for testing
@@ -69,24 +69,17 @@ def testStudent(testFileName):
     with connection.cursor() as cur:
         checker = Checker(os.getenv('DB_SCHEMA'), cur)
 
-        tests = [
-            checker.checkColumn('turniirid', 'asukoht', points=2),
-            checker.checkConstraint('partiid', 'vastavus', points=1),
-            checker.checkConstraint('isikud', 'un_isikukood', 'UNIQUE', points=0.5),
-            checker.checkConstraint('isikud', 'nimi_unique', 'UNIQUE', shouldNotExist=True, points=0.25),
-            checker.checkColumn('klubid', 'asukoht', 'character_maximum_length', 100, points=0.5),
-            checker.checkDefault('klubid', 'asukoht', None, points=1),
-        ]
-
         with open('tulemused.csv', 'a', newline='', encoding='UTF8') as file:
             writer = csv.writer(file)
 
             writer.writerow([getNameFromPath(testFileName)])
             result = 0
             for i, test in enumerate(tests):
+                testResult = checker.runTestQuery(test)
+
                 #TODO add color to ÕIGE-VALE
-                rowResult = (test[2] if test[0] else 0)
-                writer.writerow(['', i+1, ('ÕIGE' if test[0] else 'VALE'), test[1], rowResult])
+                rowResult = (testResult[2] if testResult[0] else 0)
+                writer.writerow(['', i+1, ('ÕIGE' if testResult[0] else 'VALE'), testResult[1], rowResult])
                 result += rowResult
 
             writer.writerow(['', '', '', '', result])
@@ -95,19 +88,31 @@ def testStudent(testFileName):
     #TODO cur.execute(f"DROP DATABASE {id}")
     connection.close()
 
-#TODO change w -> a
-with open('tulemused.csv', 'w', newline='', encoding='UTF8') as file:
-    writer = csv.writer(file)
-    writer.writerow(['', '', '', '', ''])
-    writer.writerow(['Praktikum3', datetime.today().strftime('%Y-%m-%d %H:%M:%S')])
 
-# Get the current directory
-directory = os.getcwd()
 
-# Loop through all files in the current directory that end with ".sql"
-for filename in os.listdir(f"{directory}/praks3"):
-    if filename.endswith('.sql'):
-        if checkIfDumpUsesCopy(f"{directory}/praks3/{filename}"):
-            print('Siin on COPY, väga halb: ' + filename)
-        else:
-            testStudent(f"{directory}/praks3/{filename}")
+def run(name, tests):
+    # TODO change w -> a
+    with open('tulemused.csv', 'w', newline='', encoding='UTF8') as file:
+        writer = csv.writer(file)
+        writer.writerow(['', '', '', '', ''])
+        writer.writerow([name, datetime.today().strftime('%Y-%m-%d %H:%M:%S')])
+
+    # Get the current directory
+    directory = os.getcwd()
+
+    # Loop through all files in the current directory that end with ".sql"
+    for filename in os.listdir(f"{directory}/praks3"):
+        if filename.endswith('.sql'):
+            if checkIfDumpUsesCopy(f"{directory}/praks3/{filename}"):
+                print('Siin on COPY, väga halb: ' + filename)
+            else:
+                testStudent(f"{directory}/praks3/{filename}", tests)
+
+run('praks3', [
+    getCheckColumnQuery('turniirid', 'asukoht', points=2),
+    getCheckConstraintQuery('partiid', 'vastavus', schema=os.getenv('DB_SCHEMA'), points=1),
+    getCheckConstraintQuery('isikud', 'un_isikukood', 'UNIQUE', schema=os.getenv('DB_SCHEMA'), points=0.5),
+    getCheckConstraintQuery('isikud', 'nimi_unique', 'UNIQUE', shouldNotExist=True, schema=os.getenv('DB_SCHEMA'), points=0.25),
+    getCheckColumnQuery('klubid', 'asukoht', 'character_maximum_length', 100, points=0.5),
+    getCheckDefaultQuery('klubid', 'asukoht', None, points=1),
+])
